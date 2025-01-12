298b72a0e69ff7a835c387cfe690470e17c7479f
rodrigo <javierluis15975@hotmail.com>
2025-01-12 15:16:23 -0500

    refactor: simplify AppService and enhance caching in AppController with Redis support

--- Dockerfile
+++ Dockerfile
@@ -1,7 +1,22 @@
 FROM node:20-alpine
+
+# Establecer el directorio de trabajo
 WORKDIR /usr/src/app
+
+# Copiar solo los archivos necesarios para instalar las dependencias
 COPY package*.json ./
-RUN npm install
+
+# Instalar las dependencias
+RUN npm install 
+
+# Copiar el resto de los archivos
 COPY . .
+
+# Compilar la aplicación
+RUN npm run build
+
+# Exponer el puerto
 EXPOSE 8000
-CMD [ "npm", "run", "start:dev" ]
+
+# Comando para iniciar la aplicación
+CMD [ "npm", "run", "start:prod" ]
--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,11 +1,32 @@
-import { Controller, Get, HttpStatus } from '@nestjs/common';
-
+import { Controller, Get, HttpStatus, Inject } from '@nestjs/common';
+import { Cacheable } from 'cacheable';
 @Controller()
 export class AppController {
+  private tasks = [
+    {
+      id: 1,
+      title: 'Task 1',
+      description: 'Description of task 1',
+    },
+  ];
+
+  constructor(@Inject('CACHE_INSTANCE') private readonly cache: Cacheable) {
+    console.log(cache);
+  }
   @Get()
-  getHello() {
+  async getHello() {
+    const tasksCache = await this.cache.get('tasks');
+    if (tasksCache)
+      return {
+        tasks: tasksCache,
+        message: 'Caching',
+        statuCode: HttpStatus.OK,
+      };
+    await this.cache.set('tasks', this.tasks);
+
     return {
-      message: 'Hello World!, in NestJS with docker compose and EC-2-AWS',
+      message: 'No caching',
+      tasks: this.tasks,
       statuCode: HttpStatus.OK,
     };
   }
