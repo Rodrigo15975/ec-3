6c18d891cc90d51f42ee2ed175e6eb5c6e2b45c4
rodrigo <javierluis15975@hotmail.com>
2025-01-12 18:41:20 -0500

    refactor: update environment variable handling in start scripts and clean up caching logic

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,5 +1,4 @@
-import { Controller, Get, HttpStatus, Inject } from '@nestjs/common';
-import { Cacheable } from 'cacheable';
+import { Controller, Get, HttpStatus } from '@nestjs/common';
 @Controller()
 export class AppController {
   private tasks = [
@@ -10,19 +9,19 @@ export class AppController {
     },
   ];
 
-  constructor(@Inject('CACHE_INSTANCE') private readonly cache: Cacheable) {
-    console.log(cache);
-  }
+  // constructor(@Inject('CACHE_INSTANCE') private readonly cache: Cacheable) {
+  //   console.log(cache);
+  // }
   @Get()
   async getHello() {
-    const tasksCache = await this.cache.get('tasks');
-    if (tasksCache)
-      return {
-        tasks: tasksCache,
-        message: 'Caching',
-        statuCode: HttpStatus.OK,
-      };
-    await this.cache.set('tasks', this.tasks);
+    // const tasksCache = await this.cache.get('tasks');
+    // if (tasksCache)
+    // return {
+    //   tasks: tasksCache,
+    //   message: 'Caching',
+    //   statuCode: HttpStatus.OK,
+    // };
+    // await this.cache.set('tasks', this.tasks);
 
     return {
       message: 'No caching',
--- src/app.module.ts
+++ src/app.module.ts
@@ -1,22 +1,22 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { Cacheable } from 'cacheable';
-import KeyvRedis from '@keyv/redis';
+// import { Cacheable } from 'cacheable';
+// import KeyvRedis from '@keyv/redis';
 @Module({
   imports: [],
   controllers: [AppController],
   providers: [
     AppService,
-    {
-      provide: 'CACHE_INSTANCE',
-      useFactory: () => {
-        const secondary = new KeyvRedis('redis://redis:6379');
+    // {
+    //   provide: 'CACHE_INSTANCE',
+    //   useFactory: () => {
+    //     const secondary = new KeyvRedis('redis://redis:6379');
 
-        return new Cacheable({ secondary, ttl: '4h' });
-      },
-    },
+    //     return new Cacheable({ secondary, ttl: '4h' });
+    //   },
+    // },
   ],
-  exports: ['CACHE_INSTANCE'],
+  // exports: ['CACHE_INSTANCE'],
 })
 export class AppModule {}
